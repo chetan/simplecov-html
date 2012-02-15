c57dcc674287c9dbb8755624441220d755e4fbb2
Chetan Sarva <chetan@pixelcop.net>
2012-02-14 22:49:43 -0500

    modify fancybox display to be almost fullscreen

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -18,51 +18,53 @@ $(document).ready(function() {
       null
 		]
   });
-  
+
   // Syntax highlight all files up front - deactivated
   // $('.source_table pre code').each(function(i, e) {hljs.highlightBlock(e, '  ')});
-  
+
   // Syntax highlight source files on first toggle of the file view popup
   $("a.src_link").click(function() {
     // Get the source file element that corresponds to the clicked element
     var source_table = $($(this).attr('href'));
-    
+
     // If not highlighted yet, do it!
     if (!source_table.hasClass('highlighted')) {
       source_table.find('pre code').each(function(i, e) {hljs.highlightBlock(e, '  ')});
       source_table.addClass('highlighted');
     };
   });
-  
+
   // Set-up of popup for source file views
   $("a.src_link").fancybox({
 		'hideOnContentClick': true,
 		'centerOnScroll': true,
-		'width': '100%',
+		'width': '99%',
+    'height': '100%',
 		'padding': 0,
-		'transitionIn': 'elastic'
+		'transitionIn': 'none',
+    'autoDimensions': false
   });
-	
+
 	// Hide src files and file list container after load
   $('.source_files').hide();
   $('.file_list_container').hide();
-  
+
   // Add tabs based upon existing file_list_containers
   $('.file_list_container h2').each(function(){
     var container_id = $(this).parent().attr('id');
     var group_name = $(this).find('.group_name').first().html();
     var covered_percent = $(this).find('.covered_percent').first().html();
-    
+
     $('.group_tabs').append('<li><a href="#' + container_id + '">' + group_name + ' ('+ covered_percent +')</a></li>');
   });
 
   $('.group_tabs a').each( function() {
     $(this).addClass($(this).attr('href').replace('#', ''));
   });
-  
+
   // Make sure tabs don't get ugly focus borders when active
   $('.group_tabs a').live('focus', function() { $(this).blur(); });
-  
+
   var favicon_path = $('link[rel="shortcut icon"]').attr('href');
   $('.group_tabs a').live('click', function(){
     if (!$(this).parent().hasClass('active')) {
@@ -71,7 +73,7 @@ $(document).ready(function() {
       $('.file_list_container').hide();
       $(".file_list_container" + $(this).attr('href')).show();
       window.location.href = window.location.href.split('#')[0] + $(this).attr('href').replace('#', '#_');
-      
+
       // Force favicon reload - otherwise the location change containing anchor would drop the favicon...
       // Works only on firefox, but still... - Anyone know a better solution to force favicon on local file?
       $('link[rel="shortcut icon"]').remove();
@@ -79,13 +81,13 @@ $(document).ready(function() {
     };
     return false;
   });
-  
+
   if (jQuery.url.attr('anchor')) {
     $('.group_tabs a.'+jQuery.url.attr('anchor').replace('_', '')).click();
   } else {
     $('.group_tabs a:first').click();
   };
-  
+
   $("abbr.timeago").timeago();
   $('#loading').fadeOut();
   $('#wrapper').show();
